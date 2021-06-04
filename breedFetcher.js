5a5aa47d84e6282c4fcc9968ea5e32e6168c0985
Andyiev <ayevdokymenmk@gmail.com>
2021-06-04 12:58:29 -0600

    breedFetcher.js last edition. breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,16 @@
 // breedFetcher.js
 const request = require('request');
 // Here is a function to organise a request to get some info from the site
-const fetchBreedDescription = function(breedName,callback) {
+const fetchBreedDescription = function (breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
     if (error) {
-      console.log("Request failed: ",error);// If the request is failed
+      callback(`Request failed: ${error}`, null);// If request failed by servers fault...
+    } else if (data.length === 0) {
+      callback("There is no cat with this name", null);//There is no cat with this breed name - we get this from the server
     } else {
-      const data = JSON.parse(body);
-      if (data.length === 0) {
-        callback(("There is no cat with name " + breedName),null); //There is no cat with this breed name, because we received empty data (empty string (array))
-      }
-      callback(null,data); // Getting information  
+      callback(null, data); // Getting information  
     }
   });
 };
-module.exports = {fetchBreedDescription};
+module.exports = { fetchBreedDescription };
--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -5,15 +5,22 @@ const { assert } = require('chai');
 
 describe('fetchBreedDescription', () => {
   it('returns a string description for a valid breed, via callback', (done) => {
-    fetchBreedDescription('Siberian', (error, data) => {
+    fetchBreedDescription('Siberian', (error, body) => {
       // we expect no error for this scenario
       assert.equal(error, null);
 
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+       // compare returned description
+      assert.equal(expectedDesc, body[0].description.trim());
+      done();
+    });
+  });
+  it('returns a note about invalid/non-existens/// breed, via callback', (done) => {
+     fetchBreedDescription('ffffff', (error, body) => {
+       console.log(error);// we expect error for this scenario
+       assert.equal(error, "There is no cat with this name");
 
-      // compare returned description
-      assert.equal(expectedDesc, data.trim());
-
+      
       done();
     });
   });
